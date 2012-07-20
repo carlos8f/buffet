443a9a6eb08a26f97729c165f54731e535f7a56d
Carlos Rodriguez <carlos@s8f.org>
2012-07-19 18:32:38 -0700

    Implement HEAD method

--- cache.js
+++ cache.js
@@ -24,19 +24,23 @@ function Cache(file, options) {
   this.stats = fs.statSync(this.file);
   this.mime = mime.lookup(this.file);
   this.buf = fs.readFileSync(file);
+
   if (/^text\//.exec(this.mime)) {
     this.gzip();
   }
+  
   this.buildHeaders();
 }
 
 Cache.prototype.buildHeaders = function() {
   this.headers = {};
   this.headers['Content-Type'] = this.mime;
+
   var charset = mime.charsets.lookup(this.file);
   if (charset) {
     this.headers['Content-Type'] += '; charset=' + charset;
   }
+
   this.headers['Last-Modified'] = this.stats.mtime.toUTCString();
   this.headers['ETag'] = hash.sha1(this.buf.toString());
   this.headers['Content-Length'] = this.stats.size;
@@ -57,20 +61,27 @@ Cache.prototype.gzip = function() {
 
 Cache.prototype.stream = function(req, res) {
   var bufProp = 'buf', headers = copy(this.headers);
+
   if (this.gzipped && req.headers['accept-encoding'] && /gzip/i.exec(req.headers['accept-encoding'])) {
     bufProp = 'gzipped';
     headers['Content-Encoding'] = 'gzip';
     headers['Content-Length'] = this.gzippedLength;
   }
+
   if (req.headers['if-none-match'] === headers['ETag'] || Date.parse(req.headers['if-modified-since']) >= this.stats.mtime) {
     delete headers['Content-Length'];
     delete headers['Content-Encoding'];
     res.writeHead(304, headers);
     res.end();
   }
+
+  res.writeHead(200, headers);
+
+  if (req.method === 'HEAD') {
+    res.end();
+  }
   else {
     headers['Date'] = new Date().toUTCString();
-    res.writeHead(200, headers);
     res.end(this[bufProp]);
   }
 };

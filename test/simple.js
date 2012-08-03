47b67ea464b8fd05b3c24e23220c58b3dc174a2b
Brian Link <cpsubrian@gmail.com>
2012-08-02 17:04:13 -0700

    Adds a test for modifying a file.

--- test/simple.js
+++ test/simple.js
@@ -128,9 +128,7 @@ describe('simple test', function() {
         fs.writeFile(testFolder + '/folder/' + folderName + '/test.json', JSON.stringify(testData), function(err) {
           assert.ifError(err);
           // Give time for the watcher to pick it up
-          setTimeout(function() {
-            done();
-          }, 1000);
+          setTimeout(done, 1000);
         });
       });
     });
@@ -151,6 +149,31 @@ describe('simple test', function() {
         });
       }).on('error', assert.ifError);
       req.end();
+    });
+
+    it('serves an updated file', function(done) {
+      testData.boo = false;
+      fs.writeFile(testFolder + '/folder/' + folderName + '/test.json', JSON.stringify(testData), function(err) {
+        assert.ifError(err);
+        // Give the watcher some time.
+        setTimeout(function() {
+          var req = http.get(baseUrl + '/folder/' + folderName + '/test.json', function(res) {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['content-type'], 'application/json');
+
+            var data = '';
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+              data += chunk;
+            });
+            res.on('end', function() {
+              assert.deepEqual(JSON.parse(data), testData);
+              done();
+            });
+          }).on('error', assert.ifError);
+          req.end();
+        }, 1000);
+      });
     });
 
     it('serves a 404 after removing dynamic file', function(done) {

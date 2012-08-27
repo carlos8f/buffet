caa477706a3ccc63ccd0b851c683b9e2626eec01
Carlos Rodriguez <carlos@s8f.org>
2012-08-27 15:43:59 -0700

    now using commander for arg parsing

--- bin/buffet.js
+++ bin/buffet.js
@@ -1,47 +1,44 @@
 #!/usr/bin/env node
-var version = require(require('path').join(__dirname, '../package.json')).version
+var program = require('commander')
+  , version = require(require('path').join(__dirname, '../package.json')).version
 
-var argv = require('optimist')
-    .alias('h', 'help')
-    .alias('p', 'port')
-    .default('p', 8080)
-    .alias('l', 'log')
-    .default('l', true)
-    .alias('v', 'version')
-    .alias('t', 'threads')
-    .default('t', require('os').cpus().length)
-    .argv
+program
+  .version(version)
+  .usage('[options] [root]')
+  .option('-r, --root <path>', 'path to webroot (default: cwd)', function (root) {
+    return root || program.args[0] || process.cwd();
+  })
+  .option('-p, --port <port>', 'specify the port (default: 8080)', Number, 8080)
+  .option('-t, --threads <count>', 'number of threads to use (default: CPU count)', Number, require('os').cpus().length)
+  .option('--no-log', 'disable logging')
+  .option('--logFile <path>', 'log requests to a file')
+  .option('--no-watch', "don't watch for file changes")
+  .option('--maxAge <seconds>', 'value for max-age Cache-Control header (default: 300)', Number, 300)
+  .option('--notFoundPath <path>', 'path of file to serve on 404 (default: /404.html)', '/404.html')
+  .option('--no-indexes', "don't serve index file when a directory is requested")
+  .option('--index <file>', 'name of index file to look for (default: index.html)', 'index.html')
+  .option('--keepAlive <ms>', 'timeout for HTTP keep-alive (default: 5000)', Number, 5000)
+  .option('--conf <path>', 'path to JSON file to load options from')
+  .parse(process.argv);
 
-if (argv.v) {
-  console.error('buffet ' + version);
-  process.exit();
-}
-else if (argv.help) {
-  console.error('Usage: buffet '
-      + '[--root=dir] [--port=port] [--no-log | --log=file...] [--no-watch]\n'
-      + '              [--conf=file...] [--max-age=seconds] [--404=404.html]\n'
-      + '              [--no-indexes] [--index=index.html] [--keep-alive=ms] [root]');
-  process.exit();
-}
-
-var options = {
-  root: argv['root'] || argv._[0] || process.cwd(),
-  watch: argv['watch'],
-  maxAge: argv['max-age'],
-  notFoundPath: argv['404'],
-  indexes: argv['indexes'],
-  index: argv['index'],
-  keepAlive: argv['keep-alive'],
-  threads: argv['threads'],
-  port: argv['port'],
-  log: argv['log']
-};
-if (argv.conf) {
-  var conf = require(argv.conf);
+if (program.conf) {
+  var conf = require(program.conf);
   Object.keys(conf).forEach(function (k) {
-    options[k] = conf[k];
+    program[k] = conf[k];
   });
 }
+
+if (program.logFile) {
+  program.log = program.logFile;
+}
+
+// Reduce commander's output to a regular object.
+var keys = Object.keys(program).filter(function (k) {
+  return !k.match(/^(commands|args|name|options|rawArgs|Command|Option)$|_/);
+}), options = {};
+keys.forEach(function (k) {
+  options[k] = program[k];
+});
 
 var cluster = require('cluster')
   , workerCount = 0
